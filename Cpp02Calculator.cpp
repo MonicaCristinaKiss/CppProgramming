0af368eac292d618c0dc4567c60e5bf8d4475dc4
Kiss Monica Cristina <71024554+MonicaCristinaKiss@users.noreply.github.com>
2020-10-18 00:49:33 +0300

    Add files via upload

---
+++ Cpp02Calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,sum;
+	cout << "Enter a number\n";
+	cin >> a;
+	cout << "Enter another number\n";
+	cin >> b;
+	sum=a+b;
+	cout << "The sum is " << sum << endl;
+	cout << "The diference is " << a-b << endl;
+	cout << "The product is " << a*b <<endl;
+	cout << a <<"/"<<b<<"= "<<a/b<<endl;
+	cout << a <<"%"<<b<<"= "<<a%b<<endl;
+	return 0;
+}
