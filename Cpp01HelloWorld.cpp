8fac98f8c7e890a63a23f130099a163e49429aa6
Kiss Monica Cristina <71024554+MonicaCristinaKiss@users.noreply.github.com>
2020-10-18 00:04:07 +0300

    Add files via upload

---
+++ Cpp01HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //preprocesor directive, includes a file that the program needs later
+using namespace std; //includes standard library
+
+int main() { //a function, the first one
+	cout << "Hello world!" << endl; //statement1
+	cout << "i love ";
+	cout << "programming\n";
+	int variabila1=1;
+	int variabila2=30;
+	int suma=variabila1+variabila2;
+	cout << suma;
+	return 0;
+}
