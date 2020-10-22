3bdbbd95b8d5c3aaaf8cab270cfa11d6db02dc16
Kiss Monica Cristina <71024554+MonicaCristinaKiss@users.noreply.github.com>
2020-10-22 12:34:58 +0300

    Add files via upload

---
+++ Cpp18Sizeof.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	float b;
+	char c;
+	double d;
+	short s;
+	int v[5];
+	cout << "sizeof int a: " << sizeof(a) << endl;
+	cout << "sizeof float b: " << sizeof(b) << endl;
+	cout << "sizeof char c: " << sizeof(c) << endl;
+	cout << "sizeof double d: " << sizeof(d) << endl;
+	cout << "sizeof short s: " << sizeof(s) << endl;
+	cout << "sizeof int v[5]: " << sizeof(v) << endl;
+	return 0;
+}
